6df695e121c3aca9c289ca554e61bf0a2f2d5624
murilommen <murilommen@gmail.com>
2022-08-16 17:23:28 -0300

    implementation logic
    details: this commit brings in two operators for the providers package whylogs. the SummaryDriftReportOperator will create a useful HTML drift report and write it to a specified location with whylogs' writers API. the ConstraintsOperator takes in some constraints definition and run it against an existing whylogs' DatasetProfile. it is able to either pass, warn or fail an Airflow DAG, depending on the configurations users pass in.

---
+++ airflow/providers/whylogs/example_dags/dag_whylogs.py
@@ -0,0 +1,89 @@
+import os
+from datetime import datetime
+import logging
+
+import pandas as pd
+import whylogs as why
+from whylogs.core.constraints.factories import (
+    greater_than_number,
+    mean_between_range,
+)
+
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.whylogs.operators.whylogs import (
+    WhylogsSummaryDriftOperator, WhylogsConstraintsOperator
+)
+
+
+def profile_data(data_path="data/transformed_data.csv"):
+    df = pd.read_csv(data_path)
+    result = why.log(df)
+    result.writer("local").write(dest="data/profile.bin")
+
+
+def transform_data(input_path="data/raw_data.csv"):
+    logging.debug(f"Current wd is {os.getcwd()}")
+    input_data = pd.read_csv(input_path)
+    clean_df = input_data.dropna(axis=0)
+    clean_df.to_csv("data/transformed_data.csv")
+
+
+def pull_args(**kwargs):
+    ti = kwargs['ti']
+    ls = ti.xcom_pull(task_ids='greater_than_check_a')
+    logging.info(ls)
+    return ls
+
+
+def read_my_dataframe():
+    df = pd.read_csv("data/raw_data.csv")
+    return df
+
+
+with DAG(
+        dag_id='whylabs_example_dag',
+        schedule_interval=None,
+        start_date=datetime.now(),
+        max_active_runs=1,
+        tags=['responsible', 'data_transformation'],
+) as dag:
+
+    transform_data = PythonOperator(
+        task_id="my_transformation",
+        python_callable=transform_data
+    )
+
+    profile_data = PythonOperator(
+        task_id="profile_data",
+        python_callable=profile_data
+    )
+
+    greater_than_check_a = WhylogsConstraintsOperator(
+        task_id="greater_than_check_a",
+        profile_path="data/profile.bin",
+        constraint=greater_than_number(column_name="a", number=0),
+    )
+    greater_than_check_b = WhylogsConstraintsOperator(
+        task_id="greater_than_check_b",
+        profile_path="data/profile.bin",
+        constraint=greater_than_number(column_name="b", number=0),
+    )
+
+    avg_between_b = WhylogsConstraintsOperator(
+        task_id="avg_between_b",
+        profile_path="data/profile.bin",
+        break_pipeline=False,
+        constraint=mean_between_range(column_name="b", lower=0.0, upper=125.1261236210),
+    )
+
+    summary_drift = WhylogsSummaryDriftOperator(
+        task_id="drift_report",
+        target_profile_path="data/profile.bin",
+        reference_profile_path="data/profile.bin",
+        reader="local",
+        write_report_path="data/Profile.html"
+    )
+
+    transform_data >> profile_data >> [greater_than_check_a, greater_than_check_b, avg_between_b] \
+        >> summary_drift
---
+++ tests/providers/whylogs/operators/test_whylogs.py
@@ -0,0 +1,105 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from whylogs.api.logger.result_set import ResultSetReader
+from whylogs.viz.extensions.reports.html_report import HTMLReportWriter
+from whylogs.core.constraints import ConstraintsBuilder, Constraints
+
+from airflow.exceptions import AirflowFailException
+from airflow.providers.whylogs.operators.whylogs import (
+    WhylogsSummaryDriftOperator,
+    WhylogsConstraintsOperator
+)
+
+TASK_ID = "test-task"
+
+PROFILE_PATH = "path/profile.bin"
+WRITE_REPORT_PATH = "some/file.html"
+
+
+class TestWhylogsSummaryDriftOperator(TestCase):
+    @patch.object(HTMLReportWriter, "write")
+    @patch.object(ResultSetReader, "read")
+    def test_execute(self, mock_read, mock_write):
+
+        op = WhylogsSummaryDriftOperator(
+            task_id=TASK_ID,
+            write_report_path=WRITE_REPORT_PATH,
+            reference_profile_path=PROFILE_PATH,
+            target_profile_path=PROFILE_PATH,
+        )
+
+        op.execute()
+        mock_read.assert_called_with(path=PROFILE_PATH)
+        self.assertEqual(mock_read.call_count, 2)
+        mock_write.assert_called_once_with(dest=WRITE_REPORT_PATH)
+
+
+class TestWhylogsConstraintsOperator(TestCase):
+    def setUp(self) -> None:
+        self.mock_custom_constraints = MagicMock(wraps=Constraints)
+
+    @patch.object(ConstraintsBuilder, "build")
+    @patch.object(ConstraintsBuilder, "add_constraint")
+    @patch("whylogs.core.constraints.factories.greater_than_number")
+    @patch.object(ResultSetReader, "read")
+    def test_execute_with_builtin_constraints(self, mock_read, mock_constraint, mock_add, mock_build):
+        op = WhylogsConstraintsOperator(
+            task_id=TASK_ID,
+            profile_path=PROFILE_PATH,
+            constraint=mock_constraint,
+            constraints=None
+        )
+        op.execute()
+        mock_read.assert_called_once_with(path=PROFILE_PATH)
+        mock_add.assert_called_once_with(mock_constraint)
+        mock_build.assert_called_once()
+
+    @patch("logging.Logger.info")
+    def test_execute_with_custom_constraints(self, mock_log):
+        self.mock_custom_constraints.validate = MagicMock(return_value=True)
+        self.mock_custom_constraints.report = MagicMock(return_value="a passing report")
+
+        op = WhylogsConstraintsOperator(
+            task_id=TASK_ID,
+            profile_path=PROFILE_PATH,
+            constraints=self.mock_custom_constraints
+        )
+        result = op.execute()
+        self.mock_custom_constraints.validate.assert_called_once()
+        self.mock_custom_constraints.report.assert_called_once()
+        self.assertEqual(result, True)
+        mock_log.assert_called_with("a passing report")
+
+    @patch("logging.Logger.warning")
+    def test_failing_but_not_breaking(self, mock_log):
+        self.mock_custom_constraints.validate = MagicMock(return_value=False)
+        self.mock_custom_constraints.report = MagicMock(return_value="a failing report")
+
+        op = WhylogsConstraintsOperator(
+            task_id=TASK_ID,
+            profile_path=PROFILE_PATH,
+            constraints=self.mock_custom_constraints,
+        )
+
+        result = op.execute()
+
+        self.mock_custom_constraints.validate.assert_called_once()
+        self.mock_custom_constraints.report.assert_called_once()
+        self.assertEqual(result, False)
+        mock_log.assert_called_with("a failing report")
+
+    @patch("logging.Logger.error")
+    def test_execute_with_break_pipeline(self, mock_error):
+        self.mock_custom_constraints.validate = MagicMock(return_value=False)
+        self.mock_custom_constraints.report = MagicMock(return_value="a failing report")
+
+        op = WhylogsConstraintsOperator(
+            task_id=TASK_ID,
+            profile_path=PROFILE_PATH,
+            constraint=None,
+            constraints=self.mock_custom_constraints,
+            break_pipeline=True
+        )
+        self.assertRaises(AirflowFailException, op.execute)
+        mock_error.assert_called_with("a failing report")
